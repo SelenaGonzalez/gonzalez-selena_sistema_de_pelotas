f2152b751b0f679481da606ebb479942abe7f866
SelenaGonzalez <selena.gonzalez@ucuenca.edu.ec>
2023-10-19 10:19:11 -0500

    se generó el sistema de pelotas y sus cambios

--- clases/Pelota.js
+++ clases/Pelota.js
@@ -1,25 +1,31 @@
 class Pelota {
   constructor() {
     this.posX = random(50, windowWidth - 50);
-    this.posY = random(50, 70);
-    this.velX = random(-1, 1);
-    this.velY = 0;
+    this.posY = random(10, 130);
+    this.velX = random(-20, 20);
+    this.velY = 0.0;
     this.acelY = 0.98;
 
-    this.diam = int(random(5, 50));
+    this.diam = int(random(5, 100));
     this.rad = this.diam / 2;
-    this.colorin = color(20, random(100, 255), random(0, 150));
-
-    print("Hola estoy vive");
+    this.colorin = color(255, random(100, 255), random(0, 150));
   }
   update(_piso) {
     if (this.posY + this.rad <= _piso) {
       this.velY += this.acelY;
       this.posY += this.velY;
     } else {
-      this.velY *= -1;
+      this.velY *= -1.0;
       this.posY += this.velY;
     }
+
+    if (this.posX > windowWidth) {
+      this.velX *= -1;
+    }
+    if (this.posX < 0) {
+      this.velX *= -1;
+    }
+    this.posX += this.velX;
   }
 
   display() {
--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
+pelotas = [];
+const NP = 100;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < NP; i++) {
+    let pelota = new Pelota();
+    pelotas.push(pelota);
+  }
+  print(pelotas);
 }
 
 function draw() {
   background(163, 99, 252);
-  noStroke();
-  let anchura = 20;
-  let gap = 50;
-
-  for (let X = 10; X < windowWidth; X += anchura + gap) {
-    for (let y = 10; y < windowHeight; y += anchura + gap) {
-      square(X, y, anchura);
-    }
+  for (let i = 0; i < NP; i++) {
+    pelotas[i].update(windowHeight);
+    pelotas[i].display();
   }
 }
